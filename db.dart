4cf70d5eb073bc4b169a81959a9536e938e2783d
Tan Yuanhong <leotan.yh@gmail.com>
2019-06-22 00:36:45 +0800

    push notification server prototype

--- db.dart
+++ db.dart
@@ -1,8 +1,21 @@
 import 'package:luminus_api/luminus_api.dart';
+import 'package:dotenv/dotenv.dart' show load, env;
 
 List<Notification> notifications = [];
 
-Map<String, List<Notification>> db = {'e0261956': []};
+Map<String, List<Notification>> db = {};
+
+Future<void> addUser(String id) async {
+  db[id] = [];
+}
+
+Future<void> removeUser(String id) async {
+  db.remove(id);
+}
+
+Future<List<String>> getUsers() async {
+  return db.keys.toList();
+}
 
 List<Notification> getUpdates(String userId, List<Notification> latest) {
   final List<Notification> stored = db[userId];
@@ -18,4 +31,12 @@ List<Notification> getUpdates(String userId, List<Notification> latest) {
   }
   db[userId] = latest;
   return updates;
+}
+
+Authentication auth = Authentication(
+      password: env['LUMINUS_PASSWORD'], username: env['LUMINUS_USERNAME']);
+
+Future<Authentication> getStoredAuth(String id) async {
+  load();
+  return id == env['LUMINUS_USERNAME'] ? auth : null;
 }
--- index.js
+++ index.js
@@ -1,30 +1,78 @@
+const express = require('express');
+const axios = require('axios');
+const external = express();
+const internal = express();
+const external_port = 3003; // port 3003 is used to receive registration and log-out requests
+const internal_port = 3004; // port 3004 is used to receive requests for sending push notifications from Dart server
+
+// DB part
+
+var db = {};
+
+function addUser(id, fcm_token) {
+    db[id] = fcm_token;
+}
+
+function removeUser(id) {
+    db[id] = undefined;
+}
+
+function getToken(id) {
+    return db[id];
+}
+
+// FCM part
+
 var admin = require("firebase-admin");
-
 var serviceAccount = require('./adminsdk_private_key.json')
-
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://f-luminus.firebaseio.com"
 });
 
-var token = "caqQvleh0uY:APA91bHv-tDE5DTo_uZf27ZvNoFTZzMtuhmMXo_l0b-uNBdjWiDns_e4F23JH4JaHqhvtx3E1BXoHxJIco6BfhEm-zrIb5p9tc4jswOJ6n30jx_xI_wwv4E6y9MT54bgXJiitfFG8_e9";
-
-var payload = {
-    notification: {
-        title: "Hello",
-        body: "World"
+function sendMessage(id, title, body) {
+    var payload = {
+        notification: {
+            title: title,
+            body: body
+        }
     }
+    var options = {
+        priority: "high",
+        timeToLive: 60 * 60 * 24
+    }
+    admin.messaging().sendToDevice(getToken(id), payload, options)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((e) => {
+            console.log(e);
+        })
 }
 
-var options = {
-    priority: "high",
-    timeToLive: 60 * 60 * 24
-}
+// Server part
 
-admin.messaging().sendToDevice(token, payload, options)
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((e) => {
-        console.log(e);
-    })
+external.get('/api/notification/activate', (req, res) => {
+    var id = req.query.id;
+    var token = req.query.fcm_token;
+    console.log('[activate] ' + 'id: ' + id + ', fcm_token: ' + token);
+    addUser(id, token);
+    axios.get('http://127.0.0.1:4004/activate', {
+        params: {
+            id: id
+        }
+    }).then((resp) => console.log('success'))
+        .catch((e) => console.log(e));
+});
+
+external.listen(external_port, () => console.log(`Externel server listening on port ${external_port}!`));
+
+internal.get('/send', (req, res) => {
+    var id = req.query.id;
+    var title = req.query.title;
+    var body = req.query.body;
+    sendMessage(id, title, body);
+    res.send('success');
+});
+
+internal.listen(internal_port, '127.0.0.1', () => console.log(`Internel server listening on port ${internal_port}!`));
