f860e3317a1a776850310ca2c15f7b9f07bc1bfb
Tan Yuanhong <leotan.yh@gmail.com>
2019-07-03 17:36:14 +0800

    Dart part is replaced with an Elixir impl

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 const express = require('express');
 const axios = require('axios');
-const external = express();
 const internal = express();
-const external_port = 3003; // port 3003 is used to receive registration and log-out requests
-const internal_port = 3004; // port 3004 is used to receive requests for sending push notifications from Dart server
-
-// DB part
-
-var db = {};
-
-function addUser(id, fcm_token) {
-    db[id] = fcm_token;
-}
-
-function removeUser(id) {
-    db[id] = undefined;
-}
-
-function getToken(id) {
-    return db[id];
-}
+const internal_port = 3004; // port 3004 is used to receive requests for sending push notifications
 
 // FCM part
 
@@ -51,21 +33,6 @@ function sendMessage(id, title, body) {
 }
 
 // Server part
-
-external.get('/api/notification/activate', (req, res) => {
-    var id = req.query.id;
-    var token = req.query.fcm_token;
-    console.log('[activate] ' + 'id: ' + id + ', fcm_token: ' + token);
-    addUser(id, token);
-    axios.get('http://127.0.0.1:4004/activate', {
-        params: {
-            id: id
-        }
-    }).then((resp) => console.log('success'))
-        .catch((e) => console.log(e));
-});
-
-external.listen(external_port, () => console.log(`Externel server listening on port ${external_port}!`));
 
 internal.get('/send', (req, res) => {
     var id = req.query.id;
